fcee0b8310317cadae9158d745f90386022cc77e
Vasanth <vasanth@Vasanths-MBP.fios-router.home>
2018-09-24 23:55:40 -0400

    parser model generated

--- src/main/java/com/ufj/parser/rest/client/Parser.java
+++ src/main/java/com/ufj/parser/rest/client/Parser.java
@@ -1,4 +1,4 @@
-package com.upj.rest.client;
+package com.ufj.parser.rest.client;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -10,12 +10,12 @@ import java.util.Base64;
 
 public class Parser {
 
-
     public static void main(String[] args) {
 
         try {
             // Specify filename and compute path
-            Path filePath = Paths.get("C:/develop/Sample.doc");
+            Path filePath = Paths.get("/Users/vasanth/develop/RParser/src/main/resources/Sample.doc");
+
 
             // Open file, encode contents to base64, then decode to UTF-8
             byte[] encoded = Base64.getEncoder().encode(Files.readAllBytes(filePath));
@@ -24,7 +24,6 @@ public class Parser {
             // Create connection object, based on the given url-name
             URL url = new URL("https://rest.resumeparsing.com/v9/parser/resume");
             HttpURLConnection connection;
-
 
             connection = (HttpURLConnection) url.openConnection();
 
@@ -39,8 +38,8 @@ public class Parser {
             connection.setRequestProperty("accept", "application/json");
 
 // Specify your credentials
-            connection.setRequestProperty("Sovren-AccountId", "55433887101");
-            connection.setRequestProperty("Sovren-ServiceKey", "6NzvycrJQr2iqOma7fwoikEDMcD2NBRRDQniRBvEVK");
+            connection.setRequestProperty("Sovren-AccountId", "55433887");
+            connection.setRequestProperty("Sovren-ServiceKey", "6NzvycrJQr2iqOma7fwoikEDMcD2NBRRDQniRBvE");
 
             // Construct payload in JSON-format
 // (This is a very primitive way to do so, as not to have any external dependencies
